979b2978347f4f0f756bcc529c6785b2f7e0935a
Aaron England <aaron.england24@gmail.com>
2019-03-18 18:37:22 -0700

    Add files via upload

--- Chapter 5/Data_Vis_1.py
+++ Chapter 5/Data_Vis_1.py
@@ -8,6 +8,8 @@ Created on Mon Mar 18 09:11:41 2019
 # data visualizations
 
 # functional method
+
+# Exercise 1: 
 import matplotlib.pyplot as plt
 
 # create an array of numbers for x
@@ -22,29 +24,27 @@ y2 = x**2
 
 # create simple line plot
 plt.figure(figsize=(10,5)) # increase plot size
-plt.plot(x, y, 'D-k', label='x cubed') # make the line between diamonds solid
-plt.plot(x, y2, '--r', label='x squared') # add another line
-plt.title('As x increases, \nx Cubed (black) increases \nat a Greater Rate than \nx Squared (red)', fontsize=22) # increase title font size
-plt.xlabel('Linearly Spaced Numbers') # add x axis label
-plt.ylabel('y Value')
-plt.legend(loc='upper left') # create a plot legend and place it in the upper left
-plt.show() # print the plot
-
-
-
-
-
 plt.plot(x, y, 'D-k', label='x cubed') # label as x cubed
 plt.plot(x, y2, '--r', label='x squared') # label as x squared
 plt.xlabel('Linearly Spaced Numbers') # add x axis label
 plt.ylabel('y Value')
-plt.title('x by x Cubed', fontsize=22) # increase title font size
+plt.title('As x increases, \nx Cubed (black) increases \nat a Greater Rate than \nx Squared (red)', fontsize=22) # make a multi-line title
 plt.legend(loc='upper left') # create a plot legend and place it in the upper left
 plt.show() # print the plot
+
+
+# Activity 1:
+
+# Exercise 2:
+
+# Activity 2:
 ###############################################################################
 
 
 # using subplots
+
+# Exercise 3:
+
 import numpy as np
 x = np.linspace(0, 10, 20)
 # create y
