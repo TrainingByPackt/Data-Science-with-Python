a3b03992ee1b5111782a6c77eb0270bc4a313811
Aaron England <aaron.england24@gmail.com>
2019-03-19 16:01:10 -0700

    Add files via upload

--- Chapter 5/Data_Vis_1.py
+++ Chapter 5/Data_Vis_1.py
@@ -9,7 +9,7 @@ Created on Mon Mar 18 09:11:41 2019
 
 # functional method
 
-# Exercise 1: 
+# Exercise 1: Line Plot
 import matplotlib.pyplot as plt
 
 # create an array of numbers for x
@@ -32,18 +32,92 @@ plt.title('As x increases, \nx Cubed (black) increases \nat a Greater Rate than 
 plt.legend(loc='upper left') # create a plot legend and place it in the upper left
 plt.show() # print the plot
 
-
-# Activity 1:
-
-# Exercise 2:
-
-# Activity 2:
 ###############################################################################
+
+# Exercise 2: Bar Plot
+import matplotlib.pyplot as plt
+
+# create a list of groups
+x = ['Shirts', 'Pants','Shorts','Shoes']
+
+# create a list of revenue
+y = [1000, 1200, 800, 1800]
+
+# create bar plot
+plt.bar(x, y) # plot revenue by group
+plt.xlabel('Item Type') # x-axis label
+plt.ylabel('Sales Revenue ($)') # y-axis label
+plt.title('Sales Revenue by Item Type')
+plt.show() # print the plot
+
+# find the index of the greatest value in list y
+index_of_max_y = y.index(max(y))
+
+# determine the most sold item
+most_sold_item = x[index_of_max_y]
+print(most_sold_item)
+
+# make the title programmatic
+plt.bar(x, y) # plot revenue by group
+plt.xlabel('Item Type') # x-axis label
+plt.ylabel('Sales Revenue ($)') # y-axis label
+plt.title('{} Produce the Most Sales Revenue'.format(most_sold_item)) # create programmatic title
+plt.show() # print the plot
+
+
+# make a horizontal bar plot
+plt.barh(x, y) # plot revenue by group
+plt.xlabel('Sales Revenue ($)') # x-axis label
+plt.ylabel('Item Type') # y-axis label
+plt.title('{} Produce the Most Sales Revenue'.format(most_sold_item)) # create programmatic title
+plt.show() # print the plot
+
+###############################################################################
+
+# Exercise 3: Histogram
+
+# generate list of normally distributed numbers
+import numpy as np
+y = np.random.normal(loc=0, scale=0.1, size=100) # 100 numbers with mean of 0 and standard deviation of 0.1
+
+# create histogram
+import matplotlib.pyplot as plt
+plt.hist(y, bins=20)
+plt.show()
+
+# run the shapiro wilk test
+from scipy.stats import shapiro
+shap_w, shap_p = shapiro(y)
+print(shap_p)
+
+# set up some logic
+if shap_p > 0.05:
+    normal_YN = 'Fail to reject the null hypothesis. Data is normally distributed.'
+else:
+    normal_YN = 'Null hypothesis is rejected. Data is not normally distributed.'
+print(normal_YN)
+
+# re-create histogram
+import matplotlib.pyplot as plt
+plt.hist(y, bins=20)
+plt.xlabel('y Value')
+plt.ylabel('Frequency')
+plt.title(normal_YN) # programmatic plot title
+plt.show()
+
+
+
+
+
+
+
+
+
 
 
 # using subplots
 
-# Exercise 3:
+# Exercise 4:Single Line Plot Using Subplots
 
 import numpy as np
 x = np.linspace(0, 10, 20)
@@ -58,8 +132,32 @@ axes.set_ylabel('y Value')
 axes.set_title('As x increases, y increases by x cubed')
 plt.show()
 
+###############################################################################
 
+# Exercise 5: Multiple Line Plots Using Subplots
+import numpy as np
+x = np.linspace(0, 10, 20)
+# create y
+y = x**3
+# create y2
+y2 = x**2
 
+# create 1 row of 2 subplots
+import matplotlib.pyplot as plt
+fig, axes = plt.subplots(nrows=1, ncols=2)
+axes[0].plot(x, y) # plot x-cubed by x
+axes[0].set_title('x by x Cubed') # set title
+axes[0].set_xlabel('Linearly Spaced Numbers') # set x axis label
+axes[0].set_ylabel('y Value') # set y axis label
+# plot on the right axes
+axes[1].plot(x, y2) # plot x-squared by x
+axes[1].set_title('x by x Squared') # set title
+axes[1].set_xlabel('Linearly Spaced Numbers') # set x axis label
+axes[1].set_ylabel('y Value') # set y axis label
+plt.tight_layout() # prevent plot overlap
+plt.show()
+
+axes[0].plot(x, y)
 
 
 
