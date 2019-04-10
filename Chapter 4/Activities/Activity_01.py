7904d378048cd2e5164e0737e2422484660634f1
Aaron England <aaron.england24@gmail.com>
2019-04-10 13:53:23 -0700

    major revisions

--- Chapter 4/Activities/Activity_01.py
+++ Chapter 4/Activities/Activity_01.py
@@ -1,4 +1,4 @@
-# Activity 1: K-Means Clustering (ensemble) 
+# Activity 1: Ensemble k-Means Clustering 
 
 # import data
 import pandas as pd
@@ -11,8 +11,7 @@ df_shuffled = shuffle(df, random_state=42)
 # standardize
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler() # create StandardScaler() object
-scaler.fit(df_shuffled) # fit scaler to the features
-scaled_features = scaler.transform(df_shuffled) # transform features to scaled version
+scaled_features = scaler.fit_transform(df_shuffled) # fit scaler model and transform df_shuffled
 
 # instantiate an empty dataframe
 import pandas as pd
@@ -20,16 +19,9 @@ labels_df = pd.DataFrame()
 
 # Build 100 models
 from sklearn.cluster import KMeans
-for i in range(1, 101):
+for i in range(100):
     model = KMeans(n_clusters=2)
     model.fit(scaled_features) # fit model
     labels = model.labels_ # get predicted labels
-    labels_df['Model_{}_Labels'.format(i)] = labels # put the labels into the empty df
-print('There are {} rows and {} columns in the labels_df data frame'.format(labels_df.shape[0], labels_df.shape[1]))
+    labels_df['Model_{}_Labels'.format(i+1)] = labels # put the labels into the empty df
 
-# now, we will get predictions across columns
-labels_df['row_mode'] = labels_df.mode(axis=1)
-
-# check out the frequency of each cluster
-import pandas as pd
-pd.value_counts(labels_df['row_mode'])
