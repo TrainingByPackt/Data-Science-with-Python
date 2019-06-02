494e1fb0f7d71bc6ce5601e5a9ddd8678d735509
Rohan Chopra <09rohanchopra@gmail.com>
2019-06-02 23:27:47 +0530

    Remove fnlwgt and use PCA instead of TSNE

--- Chapter 6/ex-3-income-script.py
+++ Chapter 6/ex-3-income-script.py
@@ -15,20 +15,19 @@ label_dict = load_obj('income_labels')
 
 age = input("Please enter age: ")
 workclass = input("Please enter workclass: ")
-fnlwgt = input("Please enter fnlwgt: ")
 education_num = input("Please enter education_num: ")
 occupation = input("Please enter occupation: ")
 capital_gain = input("Please enter capital_gain: ")
 capital_loss = input("Please enter capital_loss: ")
 hours_per_week = input("Please enter hours_per_week: ")
 
-data_list = [age, workclass, fnlwgt, education_num, occupation, capital_gain, capital_loss, hours_per_week]
+data_list = [age, workclass, education_num, occupation, capital_gain, capital_loss, hours_per_week]
 
 
 data = pd.DataFrame([data_list])
 
 
-data.columns = ['age', 'workclass', 'fnlwgt', 'education-num',
+data.columns = ['age', 'workclass', 'education-num',
        'occupation', 'capital-gain', 'capital-loss', 'hours-per-week']
 
 data[['workclass', 'occupation']] = data[['workclass', 'occupation']].apply(lambda x: label_dict[x.name].transform(x))
